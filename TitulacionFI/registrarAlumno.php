91bffe1ff98efaa82409ec8e7339e0c03c352dc4
LizParrales <LizParrales@users.noreply.github.com>
2016-04-13 19:17:29 -0500

    Delete registrarAlumno.php

--- TitulacionFI/registrarAlumno.php
+++
@@ -1,7 +0,0 @@
-<?php
-	session_start();
-	require_once("CLASSES/class.Registro.php");
-	
-	$reg = new Registro($_POST['Cuenta'], $_POST['Nombre'], $_POST['Ap_Pat'], $_POST['Ap_Mat']);
-	$reg->registrarAlumno($_POST['Creditos'], $_POST['Carrera'], $_POST['Promedio'], $_POST['Correo'], $_POST['semestre'], $_POST['Tel'], $_POST['fecha_nac']);
-?>
