e21ddd509f5c22b31976ee114dcf59c2f0d128d7
jrg sln <jrg.a.solano@gmail.com>
2016-04-14 12:13:38 -0500

    DIE

--- TitulacionFI/index.php
+++ TitulacionFI/index.php
@@ -10,6 +10,7 @@
 	<link href="css/style.css" rel="stylesheet" type="text/css" />
 	<div id="login">
 		<h1>Inicio de Sesi&oacute;n</h1>
+		<h3>DIE</h3>
 		<h3>Bienvenid@ por favor ingrese sus datos para iniciar sesi&oacute;n</h3>
 		<form method="post" action="index.php">
 			<label>Usuario</label><input name="usr" type="text" />
@@ -26,5 +27,4 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>	
 </body>
-
 </html>
